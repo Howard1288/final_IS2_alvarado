8dd6286f627c856a0138bd227baa210f95c20ac3
¨Howard <programadorhaaj.2023@gmail.com>
2023-08-05 11:59:41 -0600

    Cracion de los archivos bases de la app

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,8 @@ module.exports = {
   entry: {
     'js/app' : './src/js/app.js',
     'js/inicio' : './src/js/inicio.js',
+    'js/productos/index' : './src/js/productos/index.js',
+    'js/productos/index2' : './src/js/productos/index2.js',
   },
   output: {
     filename: '[name].js',
