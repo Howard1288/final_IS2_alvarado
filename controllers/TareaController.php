f0a5fd74943cfe8e740259375ad855c97dfdabbd
¨Howard <programadorhaaj.2023@gmail.com>
2023-08-06 17:43:51 -0600

    Actualizando datos que no corren

---
+++ controllers/TareaController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Controllers;
+
+use Exception;
+use Model\Tarea;
+use MVC\Router;
+
+class TareaController{
+    public static function index(Router $router){
+        $tarea = Tarea::all();
+
+        $router->render('tarea/index', [
+            'tarea' => $tarea,
+       
+        ]);
+
+    }
+
+    public static function guardarAPI(){
+        try {
+            $tarea = new Tarea($_POST);
+            $resultado = $tarea->crear();
+
+            if($resultado['resultado'] == 1){
+                echo json_encode([
+                    'mensaje' => 'Registro guardado correctamente',
+                    'codigo' => 1
+                ]);
+            }else{
+                echo json_encode([
+                    'mensaje' => 'Ocurrió un error',
+                    'codigo' => 0
+                ]);
+            }
+            // echo json_encode($resultado);
+        } catch (Exception $e) {
+            echo json_encode([
+                'detalle' => $e->getMessage(),
+                'mensaje' => 'Ocurrió un error',
+                'codigo' => 0
+            ]);
+        }
+    }
+
+    public static function modificarAPI(){
+        try {
+            $tarea = new Tarea($_POST);
+            $resultado = $tarea->actualizar();
+
+            if($resultado['resultado'] == 1){
+                echo json_encode([
+                    'mensaje' => 'Registro modificado correctamente',
+                    'codigo' => 1
+                ]);
+            }else{
+                echo json_encode([
+                    'mensaje' => 'Ocurrió un error',
+                    'codigo' => 0
+                ]);
+            }
+            // echo json_encode($resultado);
+        } catch (Exception $e) {
+            echo json_encode([
+                'detalle' => $e->getMessage(),
+                'mensaje' => 'Ocurrió un error',
+                'codigo' => 0
+            ]);
+        }
+    }
+
+    public static function eliminarAPI(){
+        try {
+            $tarea_id = $_POST['tarea_id'];
+            $tarea = Tarea::find($tarea_id);
+            $tarea->tarea_situacion = 0;
+            $resultado = $tarea->actualizar();
+
+            if($resultado['resultado'] == 1){
+                echo json_encode([
+                    'mensaje' => 'Registro eliminado correctamente',
+                    'codigo' => 1
+                ]);
+            }else{
+                echo json_encode([
+                    'mensaje' => 'Ocurrió un error',
+                    'codigo' => 0
+                ]);
+            }
+            // echo json_encode($resultado);
+        } catch (Exception $e) {
+            echo json_encode([
+                'detalle' => $e->getMessage(),
+                'mensaje' => 'Ocurrió un error',
+                'codigo' => 0
+            ]);
+        }
+    }
+
+    public static function buscarAPI(){
+       
+        $tarea_id_aplicacion = $_GET['tarea_id_aplicacion'];
+        $tarea_descripcion = $_GET['tarea_descripcion'];
+        $tarea_estado = $_GET['tarea_estado'];
+        $tarea_fecha = $_GET['tarea_fecha'];
+       
+
+        $sql = "SELECT * FROM tareas where tarea_situacion = 1 ";
+        if($tarea_id_aplicacion != '') {
+            $sql.= " and tarea_id_aplicacion like '%$tarea_id_aplicacion%' ";
+        }
+        if($tarea_descripcion != '') {
+            $sql.= " and tarea_descripcion like '%$tarea_descripcion%' ";
+        }
+        if($tarea_estado != '') {
+            $sql.= " and tarea_estado = $tarea_estado ";
+        }
+        if($tarea_fecha != '') {
+            $sql.= " and tarea_fecha = $tarea_fecha ";
+        }
+        try {
+            
+            $tarea = Tarea::fetchArray($sql);
+    
+            echo json_encode($tarea);
+        } catch (Exception $e) {
+            echo json_encode([
+                'detalle' => $e->getMessage(),
+                'mensaje' => 'Ocurrió un error',
+                'codigo' => 0
+            ]);
+        }
+    }
+}
--- models/Progreso_Aplicacion.php
+++ models/Progreso_Aplicacion.php
@@ -1,10 +1,6 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-require_once 'Conexion.php';
-
-class AsignacionProgramadores extends Conexion{
+namespace Model;
+class AsignacionProgramadores extends ActiveRecord{
     public $asignacion_id;
     public $asignacion_id_aplicacion;
     public $asignacion_id_programador;
@@ -16,34 +12,4 @@ class AsignacionProgramadores extends Conexion{
         $this->asignacion_id_programador = $args['asignacion_id_programador'] ?? null;
     }
 
-    public function buscar($aplicacion_id, $programador_grado, $programador_nombre, $asignacion_id)
-    {
-        $sql = "SELECT a.aplicacion_nombre, p.programador_grado, p.programador_nombre, ap.asignacion_id,
-            t.tarea_descripcion, t.tarea_estado, t.tarea_fecha
-            FROM aplicaciones a
-            JOIN asignacion_programadores ap ON a.aplicacion_id = ap.asignacion_id_aplicacion
-            JOIN programadores p ON ap.asignacion_id_programador = p.programador_id
-            JOIN tareas t ON t.tarea_id_aplicacion = a.aplicacion_id
-            WHERE 1=1";
-    
-        if (!empty($aplicacion_id)) {
-            $sql .= " AND a.aplicacion_id = $aplicacion_id";
-        }
-    
-        if (!empty($programador_grado)) {
-            $sql .= " AND p.programador_grado = '$programador_grado'";
-        }
-    
-        if (!empty($programador_nombre)) {
-            $sql .= " AND p.programador_nombre LIKE '%$programador_nombre%'";
-        }
-    
-        if (!empty($asignacion_id)) {
-            $sql .= " AND ap.asignacion_id = $asignacion_id";
-        }
-    
-        $resultados = self::servir($sql);
-    
-        return $resultados;
-    }
  }    
