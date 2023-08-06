3aa1b7fd5ee77804b2997eb98e07dec6b57d0d5a
¨Howard <programadorhaaj.2023@gmail.com>
2023-08-06 10:09:00 -0600

    Actualizando los programdores

--- models/Producto.php
+++ models/Producto.php
@@ -2,7 +2,7 @@
 
 namespace Model;
 
-class Producto extends ActiveRecord{
+class Programador extends ActiveRecord{
     public static $tabla = 'productos';
     public static $columnasDB = ['producto_nombre','producto_precio','producto_situacion'];
     public static $idTabla = 'producto_id';
--- models/Programador.php
+++ models/Programador.php
@@ -1,10 +1,10 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-require_once 'Conexion.php';
 
-class Programador extends Conexion{
+namespace Model;
+class Programador extends ActiveRecord{
+    public static $tabla = 'programadores';
+    public static $columnasDB = ['programador_grado','programador_nombre','programador_apellido','programador_situacion'];
+    public static $idTabla = 'programador_id';
     public $programador_id;
     public $programador_grado;
     public $programador_nombre;
@@ -20,47 +20,5 @@ class Programador extends Conexion{
         $this->programador_situacion = $args['programador_situacion'] ?? '';
     }
 
-    public function guardar(){
-        $sql = "INSERT INTO programadores (programador_grado, programador_nombre, programador_apellido) VALUES ('$this->programador_grado', '$this->programador_nombre', '$this->programador_apellido')";
-        $resultado = self::ejecutar($sql);
-        return $resultado;
-    }
-
-    public function buscar(){
-        $sql = "SELECT * FROM programadores WHERE programador_situacion = 1";
-
-        if($this->programador_grado != ''){
-            $sql .= " AND programador_grado LIKE '%$this->programador_grado%'";
-        }
-
-        if($this->programador_nombre != ''){
-            $sql .= " AND programador_nombre LIKE '%$this->programador_nombre%'";
-        }
-
-        if($this->programador_apellido != ''){
-            $sql .= " AND programador_apellido LIKE '%$this->programador_apellido%'";
-        }
-
-        if($this->programador_id != null){
-            $sql .= " AND programador_id = $this->programador_id";
-        }
-
-        $resultado = self::servir($sql);
-        return $resultado;
-    }
-
-    public function modificar(){
-        $sql = "UPDATE programadores SET programador_grado = '$this->programador_grado', programador_nombre = '$this->programador_nombre', programador_apellido = '$this->programador_apellido' WHERE programador_id = $this->programador_id";
-        
-        $resultado = self::ejecutar($sql);
-        return $resultado;
-    }
-
-    public function eliminar(){
-        $sql = "UPDATE programadores SET programador_situacion = 0 WHERE programador_id = $this->programador_id";
-        
-        $resultado = self::ejecutar($sql);
-        return $resultado;
-    }
 }
 ?>
